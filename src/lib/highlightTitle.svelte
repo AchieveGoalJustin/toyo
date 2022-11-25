9acd5e763aecc87e728e2fe437c0ff32e27b192f
AchieveGoalJustin <justin.smith@achievegoal.jp>
2022-11-25 17:58:33 +0900

    create the highlighted title component, still need to make the margins work with them. Margin collapse maybe?

---
+++ src/lib/highlightTitle.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let pretext=""
+	export let highlight=""
+	export let posttext=""
+</script>
+
+<div class="my-30">
+	<div>
+		<h1 class="font-bold text-4xl font-['Noto_Sans_JP'] mb-2">
+			{pretext}<span class="text-5xl font-black text-rose-700">{highlight}</span>{posttext}
+		</h1>
+	</div>
+	<hr class="h-1.5 bg-rose-700">
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,10 @@
 <script>
-	let input = 'Update The Title'
-	let title = 'Not Changed'
-	let updateTitle = () => {
-		if (input === ''){
-			input = 'Title cannot be blank'
-		} else {	
-			title = input;
-			input = '';
-		}
-	}
-
+	import Title from "$lib/highlightTitle.svelte"
 </script>
 
 <div class="container">
-	<h1 class="text-2xl font-bold">{title}</h1>
-	<hr class="bg-slate-600 h-1">
+	<Title pretext="これは" highlight="見出し" posttext="のへーディング。" />
 	<p>This is where I would put some content... <span class="text-rose-500 font-bold text-l">IF I HAD ANY!</span></p>
-	<input type="text" bind:value = {input}/>
+	<input type="text"/>
 	<button class="bg-slate-300 hover:bg-slate-600 rounded-md p-2 hover:text-white" on:click = {updateTitle}>Update Title</button>
 </div>
